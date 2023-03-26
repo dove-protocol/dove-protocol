370bc44a2bfaa7365b5d48aae0dc45a50ad9252d
soju <underrated@gmail.com>
2023-03-26 04:34:31 -0700

    reverse pool ids for USDT first

--- script/3_AddRemotesDAMM.s.sol
+++ script/3_AddRemotesDAMM.s.sol
@@ -15,7 +15,7 @@ import {Pair} from "../src/L2/Pair.sol";
 import {Configs} from "./Config.sol";
 
 contract AddRemotesDAMM is Script {
-    string RPC_ETH_GOERLI = vm.envString("ETH_GOERLI_RPC_URL");
+    string RPC_ETH_GOERLI = vm.envString("ARBITRUM_GOERLI_RPC_URL");
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("TESTNET_PRIVATE_KEY");
@@ -26,27 +26,27 @@ contract AddRemotesDAMM is Script {
         vm.createSelectFork(RPC_ETH_GOERLI);
         vm.startBroadcast(deployerPrivateKey);
 
-        L1Factory factoryL1 = L1Factory(config.factory);
+        // L1Factory factoryL1 = L1Factory(config.factory);
 
-        factoryL1.addTrustedRemote(config.dove, config.arbiDomain, bytes32(uint256(uint160(address(config.pairArbi)))));
-        factoryL1.addTrustedRemote(config.dove, config.polyDomain, bytes32(uint256(uint160(address(config.pairPoly)))));
-        factoryL1.addTrustedRemote(config.dove, config.avaxDomain, bytes32(uint256(uint160(address(config.pairAvax)))));
+        // factoryL1.addTrustedRemote(config.dove, config.arbiDomain, bytes32(uint256(uint160(address(config.pairArbi)))));
+        // factoryL1.addTrustedRemote(config.dove, config.polyDomain, bytes32(uint256(uint160(address(config.pairPoly)))));
+        // factoryL1.addTrustedRemote(config.dove, config.avaxDomain, bytes32(uint256(uint160(address(config.pairAvax)))));
 
-        L1Router routerL1 = L1Router(config.router);
+        // L1Router routerL1 = L1Router(config.router);
 
-        ERC20(config.token0).approve(address(routerL1), type(uint256).max);
-        ERC20(config.token1).approve(address(routerL1), type(uint256).max);
+        // ERC20(config.token0).approve(address(routerL1), type(uint256).max);
+        // ERC20(config.token1).approve(address(routerL1), type(uint256).max);
 
-        routerL1.addLiquidity(
-            config.token0,
-            config.token1,
-            2000000000000,
-            2000000000000,
-            2000000000000,
-            2000000000000,
-            address(msg.sender),
-            type(uint256).max
-        );
+        // routerL1.addLiquidity(
+        //     config.token0,
+        //     config.token1,
+        //     2000000000000,
+        //     2000000000000,
+        //     2000000000000,
+        //     2000000000000,
+        //     address(msg.sender),
+        //     type(uint256).max
+        // );
 
         // ERC20(config.dove).approve(address(routerL1), type(uint256).max);
 
@@ -60,7 +60,18 @@ contract AddRemotesDAMM is Script {
         //     type(uint256).max
         // );
 
-        Dove(config.dove).syncL2{value: 0.5 ether}(421613, 0x87042d892c930107615360B50D0768F514522682);
+        // Dove(config.dove).syncL2{value: 0.5 ether}(421613, 0x87042d892c930107615360B50D0768F514522682);
+
+        // L2Router(0x88BDec3893364f43194eAbe01312cd3d49AC0d2B).swapExactTokensForTokensSimple(
+        //     22000000,
+        //     21926666,
+        //     0x533046F316590C19d99c74eE661c6d541b64471C,
+        //     0x6aAd876244E7A1Ad44Ec4824Ce813729E5B6C291,
+        //     address(msg.sender),
+        //     type(uint256).max
+        // );
+
+        Pair(0x78818F4784B9A1f272F4dcDE1ED09fF182bAF407).syncToL1{value: 0.5 ether}(0.1 ether, 0.1 ether);
 
         vm.stopBroadcast();
     }
